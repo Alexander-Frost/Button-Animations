67a696c905a41418b2680539d07568ee7594e06b
Alex <alexcc17@gmail.com>
2019-04-16 00:37:08 -0400

    Initial Commit

---
+++ Button Animations/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Button Animations
+//
+//  Created by Alex on 4/16/19.
+//  Copyright © 2019 Alex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
